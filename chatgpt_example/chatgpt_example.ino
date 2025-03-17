194d5aba232f9c9b4b4f8e6de44a533d67857dd8
Dimitar <dimitar.marinov@teletek-electronics.bg>
2025-03-18 00:40:59 +0200

    Add post reques json parser

--- chatgpt_example/chatgpt_example.ino
+++ chatgpt_example/chatgpt_example.ino
@@ -1,6 +1,7 @@
 #include <WiFi.h>
 #include <WebServer.h>
 #include <Adafruit_NeoPixel.h>
+#include <ArduinoJson.h>
 
 
 // LED Setup
@@ -17,13 +18,70 @@ const char *staSSID = "AgenDuke";
 const char *staPassword = "AgenDuke@1234!";
 WebServer server(80);
 
+// Function to determine if request comes from SoftAP or STA
+bool getConnectionIsAPType(IPAddress clientIP) {
+    IPAddress apIP = WiFi.softAPIP();  // SoftAP's IP (e.g., 192.168.4.1)
+    uint32_t subnet = WiFi.softAPSubnetCIDR(); // Get SoftAP subnet mask
 
-void handleRoot() {
-//  digitalWrite(led, 1);
-  server.send(200, "text/plain", "hello from esp32!");
-//  digitalWrite(led, 0);
+    if ((clientIP & subnet) == (apIP & subnet)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
+// Function to handle JSON POST requests
+void handleRoot() {
+   IPAddress clientIP = server.client().remoteIP();  // Get client's IP address
+  bool isAPSequestType = getConnectionIsAPType(clientIP);
+  if (server.method() == HTTP_GET) {
+      pixel.setPixelColor(0, pixel.Color(128, 255, 0));
+      pixel.show();   // Turn off all pixels initially
+      server.send(200, "text/plain", "hello from esp32!");
+      pixel.setPixelColor(0, pixel.Color(255, 0, 0));
+      pixel.show();   // Turn off all pixels initially
+  }
+  else if (server.method() == HTTP_POST) {
+    if (server.hasArg("plain")) {  // Check if there's raw data
+        String jsonData = server.arg("plain");  // Get the JSON payload
+        Serial.println("Received JSON:");
+        Serial.println(jsonData);
+
+        // Create a JSON object to parse
+        StaticJsonDocument<256> doc;
+        DeserializationError error = deserializeJson(doc, jsonData);
+
+        if (error) {
+            Serial.println("JSON Parsing Failed!");
+            server.send(400, "application/json", "{\"status\":\"error\",\"message\":\"Invalid JSON\"}");
+            return;
+        }
+
+        // Extract values from JSON
+        command = doc["command"].as<String>();
+        identifier = doc["identifier"].as<int>();
+        delayTimeInSec = doc["delayTimeInSec"].as<int>();
+        workTimeInSec = doc["workTimeInSec"].as<int>();
+        coolTimeInSec = doc["coolTimeInSec"].as<int>();
+
+        // Print values to Serial Monitor
+        Serial.println("Parsed JSON Data:");
+        Serial.println("Command: " + command);
+        Serial.println("Identifier: " + String(identifier));
+        Serial.println("Delay Time: " + String(delayTimeInSec) + " sec");
+        Serial.println("Work Time: " + String(workTimeInSec) + " sec");
+        Serial.println("Cool Time: " + String(coolTimeInSec) + " sec");
+
+        // Send a success response
+        server.send(200, "application/json", "{\"status\":\"success\",\"message\":\"Command received\"}");
+    } else {
+        server.send(400, "application/json", "{\"status\":\"error\",\"message\":\"No JSON received\"}");
+    }
+  }
+  else {
+    server.send(405, "application/json", "{\"status\":\"error\",\"message\":\"Method Not Allowed\"}");
+  }
+}
 
 void setup() {
     Serial.begin(115200);
